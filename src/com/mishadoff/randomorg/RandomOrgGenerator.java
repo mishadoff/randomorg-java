13956afe8287a230a1743934bf6844a3fd02b9eb
mishadoff <mishadoff@gmail.com>
2012-06-05 11:03:21 +0300

    Basic project structure

---
+++ src/com/mishadoff/randomorg/RandomOrgGenerator.java
@@ -0,0 +1,12 @@
+package com.mishadoff.randomorg;
+
+/**
+ * Class generator provides methods
+ * for generating random numbers via athmospheric
+ * noise. Read more at {@link www.random.org}
+ * @author mishadoff
+ *
+ */
+public class RandomOrgGenerator {
+
+}
