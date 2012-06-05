104cf01402902ed560a5dd32a01d8d345e170bd8
mishadoff <mishadoff@gmail.com>
2012-06-05 15:41:52 +0300

    Integer Generator basic method added.

--- src/com/mishadoff/randomorg/IntegerGenerator.java
+++ src/com/mishadoff/randomorg/IntegerGenerator.java
@@ -1,5 +1,9 @@
 package com.mishadoff.randomorg;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  * Integer generator part of random.org API.
@@ -9,4 +13,29 @@ package com.mishadoff.randomorg;
  */
 public class IntegerGenerator {
 	
+	private final String INTEGER_QUERY = "http://www.random.org/integers/?format=plain";
+	private final String INT_MIN = "&min=";
+	private final String INT_MAX = "&max=";
+	private final String INT_NUM = "&num=";
+	private final String INT_BASE = "&base=";
+	private final String INT_COL = "&col=";
+	
+	private final int BASE = 10;
+	private final int COLS = 1;
+	
+	public ArrayList<Integer> generate(int min, int max, int count) throws IOException {
+		String query = INTEGER_QUERY 
+						+ INT_MIN + min 
+						+ INT_MAX + max
+						+ INT_NUM + count
+						+ INT_BASE + BASE
+						+ INT_COL + COLS;
+		BufferedReader br = HttpWork.get(query);
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			numbers.add(Integer.parseInt(line, BASE));
+		}
+		return numbers;
+	}
 }
